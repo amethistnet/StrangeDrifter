afabe0d0cbea6f9b7c7e4b83045ef4d0a0adb5c8
John <johnheslop06@gmail.com>
2026-01-09 23:15:32 +0000

    just the entire mod to be honest

--- StrangeDrifter/Stats.cs
+++ StrangeDrifter/Stats.cs
@@ -5,19 +5,40 @@ namespace StrangeDrifter;
 
 public static class Stats
 {
-    public static void ApplyTrashStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args)
+    public static void ApplyStrangeScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args)
     {
         if (!body.inventory)
             return;
-
-        var strangeWhiteScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapWhiteSuppressed);
         var statsFromScrapCount = body.inventory.GetItemCountEffective(DLC3Content.Items.StatsFromScrap);
 
+        args.moveSpeedMultAdd += GetWhiteScrapStats(body, args, statsFromScrapCount);
+        args.baseRegenAdd += GetGreenScrapStats(body, args, statsFromScrapCount);
+        args.attackSpeedMultAdd += GetRedScrapStats(body, args, statsFromScrapCount);
+        
+    }
+
+    private static float GetWhiteScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeWhiteScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapWhiteSuppressed);
         if (strangeWhiteScrapCount <= 0 || statsFromScrapCount <= 0)
-            return;
-
-        var bonus = 0.06f * strangeWhiteScrapCount * statsFromScrapCount;
-
-        args.moveSpeedMultAdd += bonus;
+            return 0;
+        //WHITE SCRAP CALC (short for calculator)
+        return 0.06f * strangeWhiteScrapCount * statsFromScrapCount;
+    }
+    private static float GetGreenScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeGreenScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapGreenSuppressed);
+        if (strangeGreenScrapCount <= 0 || statsFromScrapCount <= 0)
+            return 0;
+        //GREEN SCRAP
+        return statsFromScrapCount * strangeGreenScrapCount * 3f;
+    }
+    private static float GetRedScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeRedScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapRedSuppressed);
+        if (strangeRedScrapCount <= 0 || statsFromScrapCount <= 0)
+            return 0;
+        //you get the point RED SCRAP
+        return statsFromScrapCount * strangeRedScrapCount * 0.3f;
     }
 }
