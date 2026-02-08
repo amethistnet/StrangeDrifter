7a98fdabbe7a39005f8804606c1a125364c5dd43
John <johnheslop06@gmail.com>
2026-02-08 21:15:12 +0000

    config stuff and alternate buffs stuff

--- StrangeDrifter/Stats.cs
+++ StrangeDrifter/Stats.cs
@@ -10,14 +10,23 @@ public static class Stats
         if (!body.inventory)
             return;
         var statsFromScrapCount = body.inventory.GetItemCountEffective(DLC3Content.Items.StatsFromScrap);
-
-        args.moveSpeedMultAdd += GetWhiteScrapStats(body, args, statsFromScrapCount);
-        args.baseRegenAdd += GetGreenScrapStats(body, args, statsFromScrapCount);
-        args.attackSpeedMultAdd += GetRedScrapStats(body, args, statsFromScrapCount);
-        
+            args.moveSpeedMultAdd += GetStrangeWhiteScrapStats(body, args, statsFromScrapCount);
+            args.baseRegenAdd += GetStrangeGreenScrapStats(body, args, statsFromScrapCount);
+            args.attackSpeedMultAdd += GetStrangeRedScrapStats(body, args, statsFromScrapCount);
     }
 
-    private static float GetWhiteScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    public static void ApplyStrangerScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args)
+    {
+        if (!body.inventory)
+            return;
+        var statsFromScrapCount = body.inventory.GetItemCountEffective(DLC3Content.Items.StatsFromScrap);
+        //FOR THE CONFIG OPTION!!!
+        args.jumpPowerMultAdd += GetStrangerWhiteScrapStats(body, args, statsFromScrapCount);
+        args.healthMultAdd  += GetStrangerGreenScrapStats(body, args, statsFromScrapCount);
+        args.damageMultAdd += GetStrangerRedScrapStats(body, args, statsFromScrapCount);
+    }
+
+    private static float GetStrangeWhiteScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
     {
         var strangeWhiteScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapWhiteSuppressed);
         if (strangeWhiteScrapCount <= 0 || statsFromScrapCount <= 0)
@@ -25,7 +34,7 @@ public static class Stats
         //WHITE SCRAP CALC (short for calculator)
         return 0.06f * strangeWhiteScrapCount * statsFromScrapCount;
     }
-    private static float GetGreenScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    private static float GetStrangeGreenScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
     {
         var strangeGreenScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapGreenSuppressed);
         if (strangeGreenScrapCount <= 0 || statsFromScrapCount <= 0)
@@ -33,12 +42,36 @@ public static class Stats
         //GREEN SCRAP
         return statsFromScrapCount * strangeGreenScrapCount * 3f;
     }
-    private static float GetRedScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    private static float GetStrangeRedScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
     {
         var strangeRedScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapRedSuppressed);
         if (strangeRedScrapCount <= 0 || statsFromScrapCount <= 0)
             return 0;
         //you get the point RED SCRAP
         return statsFromScrapCount * strangeRedScrapCount * 0.3f;
+    }
+    private static float GetStrangerWhiteScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeWhiteScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapWhiteSuppressed);
+        if (strangeWhiteScrapCount <= 0 || statsFromScrapCount <= 0)
+            return 0;
+        //stranger white
+        return 0.04f * strangeWhiteScrapCount * statsFromScrapCount;
+    }
+    private static float GetStrangerGreenScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeGreenScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapGreenSuppressed);
+        if (strangeGreenScrapCount <= 0 || statsFromScrapCount <= 0)
+            return 0;
+        //strangest green
+        return 0.07f * strangeGreenScrapCount * statsFromScrapCount;
+    }
+    private static float GetStrangerRedScrapStats(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args, int statsFromScrapCount)
+    {
+        var strangeRedScrapCount = body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapRedSuppressed);
+        if (strangeRedScrapCount <= 0 || statsFromScrapCount <= 0)
+            return 0;
+        //strangerest red
+        return 0.14f * strangeRedScrapCount * statsFromScrapCount;
     }
 }
--- StrangeDrifter/StrangeDrifter.cs
+++ StrangeDrifter/StrangeDrifter.cs
@@ -19,7 +19,7 @@ public class StrangeDrifter : BaseUnityPlugin
     private const ItemTier whiteTier = ItemTier.Tier1;
     private const ItemTier greenTier = ItemTier.Tier2;
     private const ItemTier redTier = ItemTier.Tier3;
-    
+
     private static ConfigFile StrangeDrifterConfigFile { get; set; }
     public static ConfigEntry<bool> StrangerDrifter { get; set; }
     //surely im doing this correctly
@@ -36,9 +36,12 @@ public class StrangeDrifter : BaseUnityPlugin
 
 
         // Replace suppressed scrap tiers to match normal scrap tiers
-        var strangeWhiteScrap = LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapWhiteSuppressed));
-        var strangeGreenScrap = LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapGreenSuppressed));
-        var strangeRedScrap = LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapRedSuppressed));
+        var strangeWhiteScrap =
+            LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapWhiteSuppressed));
+        var strangeGreenScrap =
+            LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapGreenSuppressed));
+        var strangeRedScrap =
+            LegacyResourcesAPI.Load<ItemDef>("ItemDefs/" + nameof(DLC1Content.Items.ScrapRedSuppressed));
 
         // The deprecated tier is required because bad code lol
         if (strangeWhiteScrap)
@@ -95,35 +98,71 @@ public class StrangeDrifter : BaseUnityPlugin
                 body.RecalculateStats();
             };
         }
-        
+
         //
         //STRANGER SCRAP CONFIG STARTS HERE!!!
         //
         else
         {
             LanguageAPI.Add("DRIFTER_RECYCLE_TOOLTIP",
-                $"<style=cKeywordName>Recycle</style><style=cSub>Gain unique stats from each Item Scrap tier:\r\nCommon - <style=cIsUtility>+6% move speed</style> <style=cStack>(per stack)</style>.\r\nUncommon - <style=cIsHealing>+3 hp/s base health regeneration</style> <style=cStack>(per stack)</style>.\r\nLegendary - <style=cIsDamage>+30% attack speed</style> <style=cStack>(per stack)</style>.\r\nBoss - <style=cIsUtility>-15% skill cooldowns</style> <style=cStack>(per stack)</style>.\r\n\nStrange Common - <style=cIsUtility>idk man</style> <style=cStack>(per stack)</style>.\r\nStrange Uncommon - <style=cIsHealing>idk man</style> <style=cStack>(per stack)</style>.\r\nStrange Legendary - <style=cIsDamage>idk man</style> <style=cStack>(per stack)</style>.</style>");
+                $"<style=cKeywordName>Recycle</style><style=cSub>Gain unique stats from each Item Scrap tier:\r\nCommon - <style=cIsUtility>+6% move speed</style> <style=cStack>(per stack)</style>.\r\nUncommon - <style=cIsHealing>+3 hp/s base health regeneration</style> <style=cStack>(per stack)</style>.\r\nLegendary - <style=cIsDamage>+30% attack speed</style> <style=cStack>(per stack)</style>.\r\nBoss - <style=cIsUtility>-15% skill cooldowns</style> <style=cStack>(per stack)</style>.\r\n\nStrange Common - <style=cIsUtility>+4% jump height</style> <style=cStack>(per stack)</style>.\r\nStrange Uncommon - <style=cIsHealing>+7% max health</style> <style=cStack>(per stack)</style>.\r\nStrange Legendary - <style=cIsDamage>+14% damage</style> <style=cStack>(per stack)</style>.</style>");
+            RecalculateStatsAPI.GetStatCoefficients += Stats.ApplyStrangerScrapStats;
+            DrifterTrashToTreasureController.UpdateBuffCounts += (orig, self) =>
+            {
+                orig(self);
+
+                if (!NetworkServer.active)
+                    return;
+
+                var body = self.body;
+                if (!body || !body.inventory)
+                    return;
+                //white scrap buff
+                var whiteCount =
+                    body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapWhiteSuppressed);
+
+                body.SetBuffCount(DLC3Content.Buffs.TrashToTreasureWhite.buffIndex, whiteCount);
+                //green scrap buff
+                var greenCount =
+                    body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapGreenSuppressed);
+
+                body.SetBuffCount(DLC3Content.Buffs.TrashToTreasureGreen.buffIndex, greenCount);
+                //red scrap buff
+                var redCount =
+                    body.inventory.GetItemCountEffective(DLC1Content.Items.ScrapRedSuppressed);
+
+                body.SetBuffCount(DLC3Content.Buffs.TrashToTreasureRed.buffIndex, redCount);
+
+                body.RecalculateStats();
+            };
         }
     }
 
     //Uncomment for testing scrap drops
-    // private void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.F1))
-    //     {
-    //         var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-    //         PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(DLC1Content.Items.ScrapRedSuppressed.itemIndex), transform.position, transform.up * 10f);
-    //     }
-    //
-    //     if (Input.GetKeyDown(KeyCode.F2))
-    //     {
-    //         var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-    //         PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(RoR2Content.Items.ScrapWhite.itemIndex), transform.position, transform.up * 10f);
-    //     }
-    //
-    //     if (Input.GetKeyDown(KeyCode.F3))
-    //     {
-    //         var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-    //         PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(RoR2Content.Items.ScrapRed.itemIndex), transform.position, transform.up * 10f);
-    //     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+            PickupDropletController.CreatePickupDroplet(
+                PickupCatalog.FindPickupIndex(DLC1Content.Items.ScrapRedSuppressed.itemIndex), transform.position,
+                transform.up * 10f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+            PickupDropletController.CreatePickupDroplet(
+                PickupCatalog.FindPickupIndex(RoR2Content.Items.ScrapWhite.itemIndex), transform.position,
+                transform.up * 10f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+            PickupDropletController.CreatePickupDroplet(
+                PickupCatalog.FindPickupIndex(RoR2Content.Items.ScrapRed.itemIndex), transform.position,
+                transform.up * 10f);
+        }
+    }
 }
